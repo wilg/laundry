0f79d2d09101d4f03fe5f85c02e85dee159764e2
Wil Gieseler <supapuerco@gmail.com>
2012-08-07 13:29:11 -0700

    simpler login credentials

--- lib/laundry/payments_gateway/models/merchant.rb
+++ lib/laundry/payments_gateway/models/merchant.rb
@@ -28,24 +28,10 @@ module Laundry
       alias_method :clients, :client_driver
     
       def login_credentials
-        {'ticket' => MerchantAuthentication.new(@api_login_id, @api_password).login_hash}
-      end
-    
-    end
-  
-
-    class MerchantAuthentication
-    
-      def initialize(api_login_id, secure_transaction_key)
-        @api_login_id = api_login_id
-        @secure_transaction_key = secure_transaction_key
-      end
-    
-      def login_hash
         # Time diff from 1/1/0001 00:00:00 to 1/1/1970 00:00:00
         utc_time = (Time.now.to_i + 62135596800).to_s + '0000000'
-        ts_hash = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('md5'), @secure_transaction_key, "#{@api_login_id}|#{utc_time}")
-        {'APILoginID' => @api_login_id, 'TSHash' => ts_hash, 'UTCTime' => utc_time}
+        ts_hash = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('md5'), self.api_password, "#{self.api_login_id}|#{utc_time}")
+        {'ticket' => {'APILoginID' => self.api_login_id, 'TSHash' => ts_hash, 'UTCTime' => utc_time}}
       end
     
     end
